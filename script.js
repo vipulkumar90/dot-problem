b8ce71737bc494cd110f6be7d2315924f10d9d6f
Vipul Kumar <vipulchaudhary64@gmail.com>
2023-09-30 10:49:54 +0530

    added grid size ddl

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var n = 3;
+var size = 10;
 var startTime = 0;
 var endTime = 0;
-function displayCircle() {
-  hideAllCircle();
-  getAllSquare();
+function displayCircle(n) {
+  hideAllCircle(n);
+  getAllSquare(n);
   var row = Math.floor(Math.random() * n) + 1;
   var col = Math.floor(Math.random() * n) + 1;
-  console.log(row, col);
 
   document.getElementById(String(row) + String(col) + "crl").hidden = false;
-  console.log(startTime, endTime);
   document.getElementById("time").innerText =
-    String((endTime - startTime) / 1000) + "sec";
+    String(
+      (endTime - startTime) / 1000 >= 0 ? (endTime - startTime) / 1000 : 0
+    ) + "sec";
   startTime = Date.now();
 }
 
-function hideAllCircle() {
+function hideAllCircle(n) {
   for (let row = 1; row <= n; row++) {
     for (let col = 1; col <= n; col++) {
       document.getElementById(String(row) + String(col) + "crl").hidden = true;
@@ -23,7 +23,7 @@ function hideAllCircle() {
   }
 }
 
-function getAllSquare() {
+function getAllSquare(n) {
   for (let row = 1; row <= n; row++) {
     for (let col = 1; col <= n; col++) {
       document
@@ -34,11 +34,57 @@ function getAllSquare() {
               .hidden === false
           ) {
             endTime = Date.now();
-            displayCircle();
+            displayCircle(n);
           }
         });
     }
   }
 }
 
-displayCircle();
+function createGrid(n) {
+  var containter = document.getElementById("cntr");
+  var rowHtml = `
+  <span class="row" id="row">
+    {0}
+  </span>
+  `;
+  var squareHtml = `
+  <span class="square" id="{0}sq">
+    <span class="circle" hidden id="{0}crl"></span>
+  </span>
+  `;
+  var gridHtml = "";
+  for (let row = 1; row <= n; row++) {
+    var squareHtmlStr = "";
+    for (let col = 1; col <= n; col++) {
+      squareHtmlStr += formatString(squareHtml, [String(row) + String(col)]);
+    }
+    gridHtml += formatString(rowHtml, [squareHtmlStr]);
+  }
+  containter.innerHTML = gridHtml;
+}
+
+function formatString(string, params) {
+  return string.replace(/{(\d+)}/g, (match, index) => {
+    return typeof params[index] !== "undefined" ? params[index] : match;
+  });
+}
+
+function createDdl(n) {
+  var select = document.getElementById("grid-size");
+
+  for (let i = 1; i <= n; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = `${i}`;
+    select.appendChild(option);
+  }
+}
+
+var select = document.getElementById("grid-size");
+select.addEventListener("change", function () {
+  createGrid(select.value);
+  displayCircle(select.value);
+});
+
+createDdl(size);
